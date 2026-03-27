fdba3a2156fdc76402fd34c57023491ee4bf289c
giuseppetartaglia87-stack <giuseppetartaglia87@gmail.com>
2026-03-27 12:57:00 +0100

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+window.FrenchiePal = window.FrenchiePal || {};
+
+(function bootstrapFrenchiePal() {
+    const sessionId = window.FrenchiePal.getSessionId();
+    const analytics = window.FrenchiePal.createAnalytics(sessionId);
+    const trackEvent = analytics.trackEvent;
+    const trackChatOpenOnce = analytics.trackChatOpenOnce;
+
+    trackEvent('page_view');
+    window.FrenchiePal.initSectionNav();
+
+    const demoController = window.FrenchiePal.createDemoController({
+        trackEvent,
+        trackChatOpenOnce
+    });
+
+    const chatController = window.FrenchiePal.createChatController({
+        trackChatOpenOnce,
+        demoController,
+        sessionId
+    });
+
+    const waitlistController = window.FrenchiePal.createWaitlistController({
+        trackEvent,
+        sessionId
+    });
+
+    demoController.init();
+    chatController.init();
+
+    window.openDemoExperience = demoController.openDemoExperience;
+    window.toggleMobileChat = demoController.toggleMobileChat;
+    window.returnToDemoHome = demoController.returnToDemoHome;
+    window.switchTab = demoController.switchTab;
+    window.triggerAlert = demoController.triggerAlert;
+    window.setAlertScenario = demoController.setAlertScenario;
+    window.resetAlerts = demoController.resetAlerts;
+    window.triggerChat = chatController.triggerChat;
+    window.submitLeadForm = waitlistController.submitLeadForm;
+    window.trackEvent = trackEvent;
+})();
